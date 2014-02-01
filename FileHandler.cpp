3753ab00c364c35410246e491ae90469c1dc9484
Josh <ogunyinkajoshua@yahoo.com>
2014-02-01 03:41:57 -0800

    Update and rename filehandler.cpp to FileHandler.cpp
    Changes
    =======
    (1) Changed Filename to FileHandler
    (2) Implementation of new interfaces
    (3) Fixed bugs related to Windows name & path collision, including forward/backward SLASH handler in path

--- FileHandler.cpp
+++ FileHandler.cpp
@@ -1,10 +1,26 @@
-#include "./filehandler.h"
-
+#include "FileHandler.h"
 
 FileHandler::FileHandler(const std::string &_filename): filename(_filename)
 {
 	while(filename[filename.size()-1] == stringToChar(SLASH)){
 		filename.erase((filename.rbegin() + 1).base(), filename.end());
+	}
+}
+
+bool FileHandler::is_open()
+{
+	tinydir_dir dir;
+	int isOpen = tinydir_open(&dir, filename);
+	if(isOpen !=0) return false;
+	return true;
+}
+
+void FileHandler::setFilename(std::string new_file)
+{
+	filename = new_file;
+	allFiles.clear();
+	while(!directories.empty()){
+		directories.pop();
 	}
 }
 
@@ -20,17 +36,16 @@ void FileHandler::recurseAll(){
 	} while(!directories.empty());
 }
 
-void FileHandler::getSingle(){
+inline void FileHandler::getSingle(){
 	open(filename, allFiles, directories);
 }
 
-void FileHandler::open(std::string source, std::vector<std::string> &vec, std::stack<std::string> &stack)
+void FileHandler::open(const std::string &source, std::vector<std::string> &vec, std::stack<std::string> &stack)
 {
 	tinydir_dir dir;
-	std::string c(source), stack_string;
-	unsigned int i = 0;
-	tinydir_open_sorted(&dir, c.c_str());
-	for (; i < dir.n_files; i++)
+	std::string stack_string { };
+	tinydir_open_sorted(&dir, source.c_str());
+	for (unsigned int i = 0; i < dir.n_files; ++i)
 	{
 		tinydir_file file;
 		tinydir_readfile_n(&dir, &file, i);
@@ -46,30 +61,48 @@ void FileHandler::open(std::string source, std::vector<std::string> &vec, std::s
 			if(found != std::string::npos){
 					stack_string.erase(found, 2);
 			}
-			vec.push_back(stack_string + file.name);
+			vec.push_back(source + std::string(SLASH) + file.name);
 		}
 	}
 	tinydir_close(&dir);
 }
 
-std::vector<std::string> FileHandler::getFiles(){
+inline std::vector<std::string> FileHandler::getFiles(){
 	return allFiles;
 }
 
-FileHandler::size_type FileHandler::getFileNumbers(){
+inline FileHandler::size_type FileHandler::getFileNumbers(){
 	return allFiles.size();
 }
 
-char FileHandler::stringToChar(std::string s){
+inline char FileHandler::stringToChar(std::string s){
 	char c = s[0];
 	return c;
 }
 
-std::vector<std::string> FileHandler::getExtension(const std::string &ext)
+std::vector<std::string> FileHandler::getExtension(std::string ext)
 {
+	if(ext[0] != '.'){
+		ext.insert(ext.begin(), '.');
+	}
 	std::vector<std::string> temp(allFiles.cbegin(), allFiles.cend());
 	auto partition = std::stable_partition(temp.begin(), temp.end(),
 			[=](std::string s)->bool{ return s.find(ext) != std::string::npos; } );
 	temp.erase(partition, temp.end());
 	return temp;
+}
+
+bool FileHandler::isDirectory(std::string fileName)
+{
+	tinydir_dir dir;
+	tinydir_open(&dir, fileName.c_str());
+	tinydir_file file { };
+	tinydir_readfile(&dir, &file);
+
+	if (file.is_dir){
+		tinydir_close(&dir);
+		return true;
+	}
+	tinydir_close(&dir);
+	return false;
 }
